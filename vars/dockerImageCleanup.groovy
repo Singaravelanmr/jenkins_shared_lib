58b7a3be2fafd861cae2f5dfd87027f4fb88ebd4
Singaravelan <singaravelan.mr@gmail.com>
2024-01-20 09:25:56 +0530

    Update dockerImageCleanup.groovy

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -16,10 +16,8 @@
 
 
 def call(String aws_account_id, String region, String ecr_repoName) {
-    def previousBuildNumber
-        previousBuildNumber = env.BUILD_NUMBER.toInteger() - 1
         sh """
             docker rmi ${ecr_repoName}:latest || true
-            docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${previousBuildNumber} || true
+            docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest || true
         """
 }
